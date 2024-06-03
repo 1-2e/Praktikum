543229e2d017f1c9c28a1bb9c840ef3be3d10087
1-2e <holfeldj@gmail.com>
2024-06-03 10:42:02 +0200

    Add files via upload

---
+++ NetBeansProjects/Praktikum/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author 14237077
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello rld!"); 
+    }
+}
