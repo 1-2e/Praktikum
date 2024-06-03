2baf2efcd6f30871cbad58ed133bdc9e06052859
1-2e <holfeldj@gmail.com>
2024-06-03 10:51:18 +0200

    s

--- NetBeansProjects/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ NetBeansProjects/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wrnnsdfk.v fnld!");
+        System.out.println("Hello Wsdfk.v fnld!");
     }
 }
